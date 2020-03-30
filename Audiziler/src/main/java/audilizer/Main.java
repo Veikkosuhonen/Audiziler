7ebc13f64aecf7eb5b8f9aaa2cf322c2de6ffe93
Veikkosuhonen <veikko@suhonen.net>
2020-03-30 13:57:27 +0300

    harjoitustyo luotu ja viikon 3 laskarit

---
+++ Audiziler/src/main/java/audilizer/Main.java
@@ -0,0 +1,19 @@
+
+package audilizer;
+
+/**
+ *
+ * @author vesuvesu
+ */
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App.main(args);
+        
+    }
+    
+}
